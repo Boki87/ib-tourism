70df21e7914c88b6d8085e14fc4e610238de31b7
Boki87 <peric.bojan87@gmail.com>
2023-02-09 10:54:46 +0100

    fixed dev app rurl in env.local

--- environment.d.ts
+++ environment.d.ts
@@ -2,6 +2,6 @@ declare namespace NodeJS {
   export interface ProcessEnv {
     NEXT_PUBLIC_SUPABASE_URL: string;
     NEXT_PUBLIC_SUPABASE_ANON_KEY: string;
-    NEXT_PUBLIC_APP_URL: string;
+    NEXT_PUBLIC_VERCEL_URL: string;
   }
 }
--- libs/supabase.ts
+++ libs/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const APP_URL =
-  process.env.NODE_ENV === process.env.NEXT_PUBLIC_APP_URL
+  process.env.NODE_ENV === process.env.NEXT_PUBLIC_VERCEL_URL
     ? "http://192.168.0.103:3000"
     : "";
 
