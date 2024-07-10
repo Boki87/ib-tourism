09f9374fbbfb92615b14d747eab8a455426b0939
Bojan Peric <bojan@Bojans-iMac.local>
2024-07-10 13:41:27 +0200

    add youtybe shorts support

--- components/services/ServiceDrawer.tsx
+++ components/services/ServiceDrawer.tsx
@@ -74,7 +74,7 @@ export default function ServiceDrawer({
   function sanitizeVideoUrl(url: string) {
     // Check if the URL is a YouTube URL
     const youtubeRegex =
-      /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
+      /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=|shorts\/)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
 
     // Check if the URL is a Vimeo URL
     const vimeoRegex =
@@ -83,6 +83,7 @@ export default function ServiceDrawer({
     let videoId;
 
     const youtubeMatch = url.match(youtubeRegex);
+    console.log("youtube match", youtubeMatch);
     if (youtubeMatch && youtubeMatch[1]) {
       videoId = youtubeMatch[1];
       return `https://www.youtube.com/embed/${videoId}`;
